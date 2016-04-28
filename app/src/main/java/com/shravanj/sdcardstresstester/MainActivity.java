934962a5ef178f03ffbca88816e8ffb793c50149
Shravan Jambukesan <shravan.j97@gmail.com>
2016-04-28 00:35:50 -0500

    Update

--- app/src/main/java/com/shravanj/sdcardstresstester/MainActivity.java
+++ app/src/main/java/com/shravanj/sdcardstresstester/MainActivity.java
@@ -1,5 +1,6 @@
 package com.shravanj.sdcardstresstester;
 
+import android.support.v4.content.pm.ActivityInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.*;
 import android.app.*;
@@ -13,22 +14,89 @@ import java.util.*;
 public class MainActivity extends AppCompatActivity
 {
 
-    private TextView totalSD;
-    private long sdCardSpace;
-    private long time;
-    
+    private TextView sdStatus;
+    private Button startTest;
+    private Button cancelTest;
+    private Button info;
+    private ProgressBar spinner;
+    private Chronometer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        totalSD = (TextView)findViewById(R.id.totalSD);
-        sdCardSpace = getTotalSDCardSize();
+        startTest = (Button)findViewById(R.id.startTest);
+        cancelTest = (Button)findViewById(R.id.cancelTest);
+        info = (Button)findViewById(R.id.infoButton);
+        timer = (Chronometer)findViewById(R.id.timer);
+        sdStatus = (TextView)findViewById(R.id.sdStatus);
+        spinner = (ProgressBar)findViewById(R.id.progressBar);
+        displayHelpMessage();
+        stopTestHandler();
+        spinner.setVisibility(View.GONE);
         if(checkForSD())
         {
-           totalSD.setText(Long.toString(sdCardSpace) + " MB free");
+           sdStatus.setText("SD Card Detected. Press run test to start.");
+            startTest.setOnClickListener(new View.OnClickListener()
+            {
+                public void onClick(View v)
+                {
+                    sdStatus.setText("Test started");
+                    spinner.setVisibility(View.VISIBLE);
+                    timer.start();
+
+                }
+            });
         }
+        else
+        {
+            sdStatus.setText("No SD Card found");
+        }
+
+
+
+    }
+
+    public void displayHelpMessage()
+    {
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+
+            }
+        });
+    }
+
+    public void stopTestHandler()
+    {
+        cancelTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                spinner.setVisibility(View.GONE);
+                resetTimer();
+                sdStatus.setText("Test canceled");
+            }
+        });
+    }
+
+    public void resetTimer()
+    {
+        long stopped = timer.getBase() - SystemClock.elapsedRealtime();
+        timer.setBase(SystemClock.elapsedRealtime());
+        timer.stop();
+        stopped = 0;
+    }
+
+    public void createFiles()
+    {
+
+    }
+
+    public void deleteFiles()
+    {
 
     }
 
@@ -44,14 +112,6 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    public long getTotalSDCardSize()
-    {
-        File f = Environment.getExternalStorageDirectory();
-        StatFs stat = new StatFs(f.getPath());
-        long size = stat.getBlockSize();
-        long count = stat.getBlockCount();
 
-        return (size * count) / (long) (Math.pow(1024, 2));
 
-    }
 }
