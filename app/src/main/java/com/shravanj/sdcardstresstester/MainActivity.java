4822b29be5debf83da26c00df0080feb91146c0e
Shravan Jambukesan <shravan.j97@gmail.com>
2016-04-29 18:34:03 -0500

    updated gui elements

--- app/src/main/java/com/shravanj/sdcardstresstester/MainActivity.java
+++ app/src/main/java/com/shravanj/sdcardstresstester/MainActivity.java
@@ -1,13 +1,16 @@
 package com.shravanj.sdcardstresstester;
 
+import android.app.AlertDialog;
 import android.support.v4.content.pm.ActivityInfoCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.*;
 import android.os.*;
 import android.app.*;
 import android.view.*;
 import android.widget.*;
+import android.content.*;
 import java.io.*;
 import java.util.*;
+
 
 
 
@@ -20,6 +23,10 @@ public class MainActivity extends AppCompatActivity
     private Button info;
     private ProgressBar spinner;
     private Chronometer timer;
+    private boolean testInitiated;
+    private final int NUM_FILES_TO_GENERATE = 1000;
+    private int fileNumber;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -33,16 +40,18 @@ public class MainActivity extends AppCompatActivity
         sdStatus = (TextView)findViewById(R.id.sdStatus);
         spinner = (ProgressBar)findViewById(R.id.progressBar);
         displayHelpMessage();
-        stopTestHandler();
         spinner.setVisibility(View.GONE);
         if(checkForSD())
         {
+            final long sdAvailableSpace = getTotalSDCardSize();
            sdStatus.setText("SD Card Detected. Press run test to start.");
             startTest.setOnClickListener(new View.OnClickListener()
             {
                 public void onClick(View v)
                 {
-                    sdStatus.setText("Test started");
+                    sdStatus.setText("Test started. SD avail: " + sdAvailableSpace);
+                    testInitiated = true;
+                    stopTestHandler();
                     spinner.setVisibility(View.VISIBLE);
                     timer.start();
 
@@ -64,22 +73,38 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onClick(View view)
             {
-
+                createHelpMessage();
             }
         });
     }
 
+    public void createHelpMessage()
+    {
+        AlertDialog help = new AlertDialog.Builder(MainActivity.this).create();
+        help.setTitle("Info");
+        help.setMessage("Running the test will simulate wear by generating files that take up 10% of the available SD card space, time it, delete the files, time the deletion, and finally list the results. This can simulate heavy wear if used multiple times.");
+        help.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        help.show();
+    }
+
     public void stopTestHandler()
     {
-        cancelTest.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view)
-            {
-                spinner.setVisibility(View.GONE);
-                resetTimer();
-                sdStatus.setText("Test canceled");
-            }
-        });
+        if(testInitiated)
+        {
+            cancelTest.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    spinner.setVisibility(View.GONE);
+                    resetTimer();
+                    sdStatus.setText("Test canceled");
+                }
+            });
+        }
     }
 
     public void resetTimer()
@@ -110,6 +135,13 @@ public class MainActivity extends AppCompatActivity
         {
             return false;
         }
+    }
+
+    public long getTotalSDCardSize()
+    {
+        StatFs sdstat = new StatFs(Environment.getExternalStorageDirectory().getPath());
+        long availableSpace = (long) sdstat.getAvailableBlocks() * (long) sdstat.getBlockCount() / (long) Math.pow(1024, 2);
+        return availableSpace;
     }
 
 
